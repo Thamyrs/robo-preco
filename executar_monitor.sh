d84e524a1db5163661deee3eb07f2ef1624c7d8e
user <thamyrs.carvalho@gmail.com>
2025-11-12 12:57:14 -0300

    feat: criacao de script de execucao bash para automacao em ambiente Linux

---
+++ executar_monitor.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PROJETO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV_DIR="$PROJETO_DIR/venv"
+REQ_FILE="$PROJETO_DIR/requirements.txt"
+SCRIPT="$PROJETO_DIR/monitora_preco.py"
+LOG_DIR="$PROJETO_DIR/logs"
+
+mkdir -p "$LOG_DIR"
+
+LOG_FILE="$LOG_DIR/script_linux_$(date +'%Y-%m-%d_%H-%M-%S').log"
+
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+echo "================================================"
+echo "Iniciando execução do robô de preços..."
+echo "Data/Hora: $(date)"
+echo "================================================"
+
+if ! command -v python3 &>/dev/null; then
+    echo "Python 3 não encontrado! Instale com: sudo apt install python3 python3-venv -y"
+    exit 1
+fi
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Criando ambiente virtual..."
+    python3 -m venv "$VENV_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Falha ao criar ambiente virtual."
+        exit 1
+    fi
+fi
+
+source "$VENV_DIR/bin/activate"
+
+if [ -f "$REQ_FILE" ]; then
+    echo "Instalando dependências do requirements.txt..."
+    pip install -r "$REQ_FILE"
+fi
+
+echo "Iniciando execução do script Python..."
+python3 "$SCRIPT"
+
+
+if [ $? -eq 0 ]; then
+    echo "Execução concluída com sucesso!"
+else
+    echo "Ocorreu um erro durante a execução do script."
+fi
+
+deactivate
+exit 0
